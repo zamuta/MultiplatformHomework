3c2bfb17655560ddf6b0136d77b38257aeb8f51f
kanshin.m <zamuta@gmail.com>
2022-07-11 18:12:30 +0700

    - Подключен композ

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     kotlin("android")
 }
 
+val composeVersion = "1.2.0"
+val uiComposeVersion = "1.1.1"
+
 android {
     compileSdk = 32
     defaultConfig {
@@ -11,10 +14,32 @@ android {
         targetSdk = 32
         versionCode = 1
         versionName = "1.0"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
+        }
+    }
+    // Added for Jetpack Compose
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
@@ -24,4 +49,13 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    // Added for Jetpack Compose
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.compose.material:material:$uiComposeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$uiComposeVersion")
+    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["compose_version"]}")
 }
--- androidApp/src/main/java/com/example/multiplatformhomework/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multiplatformhomework/android/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.multiplatformhomework.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.example.multiplatformhomework.Greeting
-import android.widget.TextView
 
 fun greet(): String {
     return Greeting().greeting()
 }
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            MaterialTheme {
+                MainView()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainView() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = greet())
     }
 }
