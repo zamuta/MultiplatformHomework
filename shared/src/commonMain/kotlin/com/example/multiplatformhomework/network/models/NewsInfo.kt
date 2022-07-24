2fa55572f640708c578669d59b724eeb06074ea8
kanshin.m <zamuta@gmail.com>
2022-07-24 17:46:12 +0700

    - Фикс ключей

--- shared/src/commonMain/kotlin/com/example/multiplatformhomework/network/models/NewsInfo.kt
+++ shared/src/commonMain/kotlin/com/example/multiplatformhomework/network/models/NewsInfo.kt
@@ -9,9 +9,9 @@ data class NewsInfo(
     @Required val description: kotlin.String,
     @Required val url: kotlin.String,
     @Required val source: kotlin.String,
-    val imageUrl: kotlin.String? = null,
+    @SerialName(value = "image_url") val imageUrl: kotlin.String? = null,
     val language: kotlin.String? = null,
-    val publishedAt: kotlin.String? = null,
+    @SerialName(value = "published_at") val publishedAt: kotlin.String? = null,
     val categories: kotlin.collections.List<kotlin.String>? = null
 )
 
